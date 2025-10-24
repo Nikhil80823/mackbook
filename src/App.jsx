0d4e29af8695718627b9b4cea64fc13ecc92e3d2
Nikhil Bhadwal <nikhilbhadwal808@gmail.com>
2025-10-24 10:36:12 +0530

    Implement Performance section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import ProductViewer from './components/ProductViewer'
 import gsap from 'gsap'
 import {ScrollTrigger,} from "gsap/ScrollTrigger";
 import Showcase from './components/Showcase'
+import Features from './components/Features'
+import Highlights from './components/Highlights'
+import Footer from './components/Footer'
+import Performance from './components/Performance'
 
 gsap.registerPlugin(ScrollTrigger)
 const App = () => {
@@ -14,6 +18,10 @@ const App = () => {
     <Hero/>
     <ProductViewer/>
     <Showcase/>
+    <Performance/>
+    <Features/>
+    <Highlights/>
+    <Footer/>
     </main>
   )
 }
